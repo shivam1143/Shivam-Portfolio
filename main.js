bb2c0b2e2f53855e78adbf1a22966024540f5fef
shivam1143 <158501028+shivam1143@users.noreply.github.com>
2024-02-26 22:03:43 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,34 @@
+var typed = new Typed(".text", {
+    strings: ["Frontend Developer" , "Web Developer"],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop: true
+    });
+
+
+
+ 'use strict';
+
+function progressBarAndCountNumber () {
+    const progress = document.querySelectorAll('.progress');
+    let count = 0;
+    // You must put the maximum number in the MAX variable.
+    let MAX = 95;
+
+    let run = setInterval(() => {
+        count++;
+        progress.forEach(element => {
+            if (count <= element.dataset.progress) {
+                element.parentElement.style.background = `conic-gradient(#0ef ${count}%, #212428 0)`;
+                element.firstElementChild.textContent = `${count}%`;
+            };
+            if (count == MAX) {
+                clearInterval(run);
+            };
+        });
+    }, 20);
+}
+
+progressBarAndCountNumber();
+        
